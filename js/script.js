3ea4665078614878a23d45e12015e36454b8ea30
Riti <ritikarthikeyan@gmail.com>
2026-05-26 21:28:46 -0700

    Add ship selection and travel functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+function chooseShip(shipImage){
 
+    const ship = document.getElementById("chosen-ship");
+
+    ship.src = shipImage;
+
+    document.getElementById("ship-screen").style.display = "none";
+
+    document.getElementById("map-screen").style.display = "block";
+}
+
+function travelTo(page, x, y){
+
+    const ship = document.getElementById("chosen-ship");
+
+    ship.style.left = x;
+
+    ship.style.top = y;
+
+    setTimeout(function(){
+        window.location.href = page;
+    }, 2000);
+}
